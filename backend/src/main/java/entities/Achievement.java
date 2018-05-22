ebc93e05e730c211b83bce918c0ae623ca368e97
haxhaxdax <sauermax42@gmail.com>
2018-05-22 14:00:27 +0200

    updated Achievements

--- backend/src/main/java/entities/Achievement.java
+++ backend/src/main/java/entities/Achievement.java
@@ -1,6 +1,56 @@
 package entities;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Achievement extends EntityGeneratedKey{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(updatable = false, nullable = false)
+	int achievementId;
+	
+	@Column(updatable = true, nullable = false)
+	String achievementName;
+	@Column(updatable = true, nullable = false)
+	String achievementDescription;
+	@Column(updatable = true, nullable = false)
+	int iconId;
+	@Column(updatable = true, nullable = false)
+	String Title;
+	
+	public int getAchievementId() {
+		return achievementId;
+	}
+	public void setAchievementId(int achievementId) {
+		this.achievementId = achievementId;
+	}
+	public String getAchievementName() {
+		return achievementName;
+	}
+	public void setAchievementName(String achievementName) {
+		this.achievementName = achievementName;
+	}
+	public String getAchievementDescription() {
+		return achievementDescription;
+	}
+	public void setAchievementDescription(String achievementDescription) {
+		this.achievementDescription = achievementDescription;
+	}
+	public int getIconId() {
+		return iconId;
+	}
+	public void setIconId(int iconId) {
+		this.iconId = iconId;
+	}
+	public String getTitle() {
+		return Title;
+	}
+	public void setTitle(String title) {
+		Title = title;
+	}
+	
 }
