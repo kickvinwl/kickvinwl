6d7d68a71147ab638df345eac902ac9669de3954
G Back <27279305+g-back@users.noreply.github.com>
2018-05-22 14:27:26 +0200

    updated match

--- backend/src/main/java/entities/Match.java
+++ backend/src/main/java/entities/Match.java
@@ -1,7 +1,21 @@
 package entities;
-import javax.persistence.Entity;
+import org.joda.time.DateTime;
+
+import javax.persistence.*;
 
 @Entity
 public class Match extends EntityExternalKey{
+
+    private int matchdayId;
+    private DateTime matchDateTime;
+    @ManyToMany
+    @JoinColumn(name = "id")
+    private Team team1Id;
+    @ManyToMany
+    @JoinColumn(name = "id")
+    private Team team2Id;
+    private int goalsTeam1;
+    private int goalsTeam2;
+    private int statusId;
 
 }
