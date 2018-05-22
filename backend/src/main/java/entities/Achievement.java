82f7559bf81e81b42bedaf74a4a7ee14b44cc09f
haxhaxdax <sauermax42@gmail.com>
2018-05-22 14:13:39 +0200

    added AchievementIcon

--- backend/src/main/java/entities/Achievement.java
+++ backend/src/main/java/entities/Achievement.java
@@ -1,33 +1,20 @@
 package entities;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Entity
 public class Achievement extends EntityGeneratedKey{
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(updatable = false, nullable = false)
-	int achievementId;
+		
+	@Column(updatable = true, nullable = false)
+	private String achievementName;
+	@Column(updatable = true, nullable = false)
+	private String achievementDescription;
+	@Column(updatable = true, nullable = false)
+	private int iconId;
+	@Column(updatable = true, nullable = false)
+	private String Title;
 	
-	@Column(updatable = true, nullable = false)
-	String achievementName;
-	@Column(updatable = true, nullable = false)
-	String achievementDescription;
-	@Column(updatable = true, nullable = false)
-	int iconId;
-	@Column(updatable = true, nullable = false)
-	String Title;
-	
-	public int getAchievementId() {
-		return achievementId;
-	}
-	public void setAchievementId(int achievementId) {
-		this.achievementId = achievementId;
-	}
 	public String getAchievementName() {
 		return achievementName;
 	}
