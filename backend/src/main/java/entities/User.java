21796048ba1126aa450304ec4894e6df0f244440
haxhaxdax <sauermax42@gmail.com>
2018-05-23 09:09:31 +0200

    updated newsfeedmessage

--- backend/src/main/java/entities/User.java
+++ backend/src/main/java/entities/User.java
@@ -1,5 +1,61 @@
 package entities;
 
-public class User extends EntityGeneratedKey {
+import javax.persistence.Column;
 
+public class User extends EntityGeneratedKey {
+	@Column(updatable = true, nullable = false)
+	private String userName;
+	@Column(updatable = true, nullable = false)
+	private String userPicture;
+	@Column(updatable = true, nullable = false)
+	private boolean userIsAdmin;
+	
+	//FK zu Achievement
+	@Column(updatable = true, nullable = false)
+	private String displayedTitle;
+	
+	@Column(updatable = true, nullable = false)
+	private String sessionKey;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserPicture() {
+		return userPicture;
+	}
+
+	public void setUserPicture(String userPicture) {
+		this.userPicture = userPicture;
+	}
+
+	public boolean isUserIsAdmin() {
+		return userIsAdmin;
+	}
+
+	public void setUserIsAdmin(boolean userIsAdmin) {
+		this.userIsAdmin = userIsAdmin;
+	}
+
+	public String getDisplayedTitle() {
+		return displayedTitle;
+	}
+
+	public void setDisplayedTitle(String displayedTitle) {
+		this.displayedTitle = displayedTitle;
+	}
+
+	public String getSessionKey() {
+		return sessionKey;
+	}
+
+	public void setSessionKey(String sessionKey) {
+		this.sessionKey = sessionKey;
+	}
+	
+	
 }
