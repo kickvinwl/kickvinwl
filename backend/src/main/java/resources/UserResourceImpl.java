7804992802dd3138a3f6d8cc2c03a507c737f756
Biotrade <karl-runz@gmx.de>
2018-05-29 09:54:45 +0200

    added impl class for userResource

---
+++ backend/src/main/java/resources/UserResourceImpl.java
@@ -0,0 +1,19 @@
+package resources;
+
+import entities.User;
+
+import javax.ws.rs.core.Response;
+
+public class USerResourceImpl extends UserResource {
+
+
+    @Override
+    public User getUser() {
+        return null;
+    }
+
+    @Override
+    public Response setUser() {
+        return null;
+    }
+}
