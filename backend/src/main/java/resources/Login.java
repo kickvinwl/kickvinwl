311b65d735bb334d5846076e339c690ebf321f8d
Mankianer <marvin.woelk@web.de>
2018-05-30 14:17:11 +0200

    User anlegen in der DB

--- backend/src/main/java/resources/Login.java
+++ backend/src/main/java/resources/Login.java
@@ -3,12 +3,15 @@ package resources;
 
 import de.kvwl.commons.authentication.AuthenticationServiceFactory;
 import entities.User;
+import jdk.nashorn.internal.runtime.regexp.joni.constants.AsmConstants;
 import persistence.UserPersistenceService;
 
 
+import javax.persistence.NoResultException;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.UUID;
 
@@ -24,7 +27,7 @@ public class Login {
         Response.ResponseBuilder rb = Response.accepted();
 
         boolean isAllow = AuthenticationServiceFactory.getInstance().isUserInGroup(name, pw, group);
-        String token;
+        HashMap hmap = new HashMap<String, String>();
 
         if(isAllow)
         {
@@ -32,25 +35,27 @@ public class Login {
 
                 //User finden/erstellen
                 User user = UserPersistenceService.getInstance().getByName(name);
-                if( user == null)
-                {
-                    rb.status(Response.Status.NO_CONTENT);
-                }
-                else {
-                    //token generieren
-                    token = generateToken();
 
-                    //token setzten
-                    HashMap hmap = new HashMap<String, String>();
-                    hmap.put("token", token);
-                    rb.entity(hmap);
-                    user.setSessionKey(token);
+                //token generieren
+
+                //token setzten
+                user.setSessionKey(generateToken());
+                hmap.put("token", user.getSessionKey());
+                rb.entity(hmap);
                     //TODO Last login setzten für User
 
                     //User speichern
                     UserPersistenceService.getInstance().update(user);
-                }
-            } catch (Exception e)
+
+            }
+            catch (NoResultException re)
+            {
+                rb.status(Response.Status.NO_CONTENT); //TODO hier nutzer anlegen
+                System.out.println("Neuer Nutzer angelegt:");
+                hmap.put("token", createNewUser(name));
+                rb.entity(hmap);
+            }
+            catch (Exception e)
             {
                 e.printStackTrace();
             }
@@ -61,6 +66,27 @@ public class Login {
         return rb.build();
     }
 
+    /**
+     *
+     * @param userName
+     * @return Token
+     */
+    private String createNewUser(String userName)
+    {
+        User user = new User();
+        user.setUserName(userName);
+        user.setUserPicture("default");
+        user.setUserIsAdmin(userName == "Woelk_m");
+        user.setSessionKey(generateToken());
+        user.setLastAction(new Date());
+        //TODO user daten füllen
+
+        UserPersistenceService.getInstance().save(user);
+
+        return user.getSessionKey();
+    }
+
+    @Deprecated
     @GET
     @Path("/logout/{sessionKey}")
     public Response getUserBySessionKey(@PathParam("sessionKey") String sessionKey) {
