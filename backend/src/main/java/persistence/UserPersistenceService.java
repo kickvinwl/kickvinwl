a3ccc061b8e477747a116608e30ac43b42520ef9
G Back <27279305+g-back@users.noreply.github.com>
2018-05-30 14:03:26 +0200

    removed save del upd

--- backend/src/main/java/persistence/UserPersistenceService.java
+++ backend/src/main/java/persistence/UserPersistenceService.java
@@ -15,6 +15,11 @@ public class UserPersistenceService extends PersistenceService<User> {
 
     private static UserPersistenceService instance;
 
+    public static UserPersistenceService getInstance()
+    {
+        return instance = instance != null ? instance : new UserPersistenceService();
+    }
+
     public User getByName(final String userName) throws NoResultException {
         return JPAOperations.doInJPA(this::entityManagerFactory, entityManager -> {
             Query query = entityManager.createQuery("SELECT us FROM User us WHERE userName = :userName");
@@ -68,15 +73,6 @@ public class UserPersistenceService extends PersistenceService<User> {
         });
     }
 
-    public void delete(final User user) throws EntityNotFoundException{
-        JPAOperations.doInJPA(this::entityManagerFactory, entityManager -> {
-            Query query = entityManager.createQuery("DELETE FROM User WHERE userName = :userName");
-            query.setParameter("userName", user.getUserName());
-            int deleted = query.executeUpdate();
-            if (deleted == 0)
-                throw new EntityNotFoundException();
-        });
-    }
     public void deleteByUserName(final String userName) throws EntityNotFoundException{
         JPAOperations.doInJPA(this::entityManagerFactory, entityManager -> {
             Query query = entityManager.createQuery("DELETE FROM User WHERE userName = :userName");
@@ -87,46 +83,12 @@ public class UserPersistenceService extends PersistenceService<User> {
         });
     }
 
-    public void update(final User user) throws EntityNotFoundException{
-        JPAOperations.doInJPA(this::entityManagerFactory, entityManager -> {
-
-            Query query = entityManager.createQuery("UPDATE User SET userPicture = :pic, userIsAdmin = :admin, displayedTitle = :title, sessionKey = :session WHERE userName= :userName");
-//			query.setParameter("pic", user.getDisplayedTitle());
-
-            query.setParameter("admin", user.isUserIsAdmin());
-            //	query.setParameter("title", user.getDisplayedTitle());
-            query.setParameter("session", user.getSessionKey());
-            query.setParameter("userName", user.getUserName());
-            int updated = query.executeUpdate();
-            if (updated == 0)
-                throw new EntityNotFoundException();
-        });
-    }
     @SuppressWarnings("unchecked")
     public List<Group> getGroups(final User user) throws EntityNotFoundException{
         return JPAOperations.doInJPA(this::entityManagerFactory, entityManager -> {
             Query query = entityManager.createQuery("SELECT g FROM Group g INNER JOIN GroupUser gu ON g.id=gu.fk_group WHERE gu.fk_user = :userId");
             query.setParameter("userId", user.getId());
             return query.getResultList();
-        });
-    }
-
-    public void save(final User user) throws EntityExistsException {
-        JPAOperations.doInJPA(this::entityManagerFactory, entityManager -> {
-            try {
-                this.getByName(user.getUserName());
-                throw new EntityExistsException();
-            }catch(NoResultException e) {
-                //User nicht gefunden, also darf er erstellt werden
-                Query query = entityManager.createQuery("INSERT INTO User (userPicture, isUserAdmin, displayedTitle, sessionKey) VALUES (:pic, :admin, :title, :session)");
-                //	query.setParameter("pic", user.getDisplayedTitle());
-                query.setParameter("admin", user.isUserIsAdmin());
-                //	query.setParameter("title", user.getDisplayedTitle());
-                query.setParameter("session", user.getSessionKey());
-                query.setParameter("userName", user.getUserName());
-                return query.executeUpdate();
-            }
-
         });
     }
 
@@ -151,8 +113,6 @@ public class UserPersistenceService extends PersistenceService<User> {
         });
     }
 
-
-
     @SuppressWarnings("unchecked")
     public List<User> getAll() {
         return JPAOperations.doInJPA(this::entityManagerFactory, entityManager -> {
@@ -161,9 +121,4 @@ public class UserPersistenceService extends PersistenceService<User> {
         });
     }
 
-
-    public static UserPersistenceService getInstance()
-    {
-        return instance = instance != null ? instance : new UserPersistenceService();
-    }
 }
