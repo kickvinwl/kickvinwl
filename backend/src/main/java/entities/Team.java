10819c0f53c3dd527c2c30ad297df66dae061a25
G Back <27279305+g-back@users.noreply.github.com>
2018-06-05 13:24:36 +0200

    implemented team entity

--- backend/src/main/java/entities/Team.java
+++ backend/src/main/java/entities/Team.java
@@ -5,4 +5,30 @@ import javax.persistence.Entity;
 @Entity
 public class Team extends EntityGeneratedKey {
 	private int teamId;
+	private String teamName;
+	private String teamIconURL;
+
+	public int getTeamId() {
+		return teamId;
+	}
+
+	public void setTeamId(int teamId) {
+		this.teamId = teamId;
+	}
+
+	public String getTeamName() {
+		return teamName;
+	}
+
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+
+	public String getTeamIconURL() {
+		return teamIconURL;
+	}
+
+	public void setTeamIconURL(String teamIconURL) {
+		this.teamIconURL = teamIconURL;
+	}
 }
