3de973f99d573d73c515fe38f698a52f8bcc4dd7
Biotrade <karl-runz@gmx.de>
2018-06-06 11:44:21 +0200

    added data model for tips - Tip Resource

--- backend/src/main/java/resources/TipResource.java
+++ backend/src/main/java/resources/TipResource.java
@@ -2,19 +2,21 @@ package resources;
 
 import entities.MatchTip;
 import entities.User;
+import resources.datamodel.Tip;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
 
 @Path("/{a:tip|Tip}")
 @Consumes(MediaType.APPLICATION_JSON)
 public abstract class TipResource {
-
+    Response response;
 
     @POST
     @Path("/{a:set|Set}")
-    public abstract Response setTip(@QueryParam("sessionKey") String token);
+    public abstract Response setTip(String token, int gameday, String season, ArrayList<Tip> tipList);
 
     @GET
     @Path("/{a:get|Get}/{token}")
--- backend/src/main/java/resources/TipResourceImpl.java
+++ backend/src/main/java/resources/TipResourceImpl.java
@@ -1,26 +1,33 @@
 package resources;
 
-import entities.Match;
+import com.sun.xml.internal.bind.v2.TODO;
 import entities.MatchTip;
 import entities.User;
 import persistence.MatchTipPersistenceService;
 import persistence.UserPersistenceService;
+import resources.datamodel.Tip;
 
+import javax.persistence.NoResultException;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TipResourceImpl extends TipResource {
 
     @Override
-    public Response setTip(String token) {
-        Response.ResponseBuilder response = Response.accepted();
+    public Response setTip(String token, int gameday, String season, ArrayList<Tip> tipList) {
+        response = Response.accepted().build();
 
-        //Token -> User
+        try {
+            UserPersistenceService.getInstance().getBySessionKey(token);
+            // TODO: Persistieren der Matches
+            response = Response.accepted().build();
+        }
+        catch (SecurityException | NoResultException exception) {
+            response = Response.status(Response.Status.BAD_REQUEST).build();
+        }
 
-        //
-
-
-        return response.build();
+        return response;
     }
 
 
