abed58a6f2a007d7278bfb64f2cda32b318d1915
g-back <27279305+g-back@users.noreply.github.com>
2018-06-12 11:21:28 +0200

    todo added

--- backend/src/main/java/entities/Matchday.java
+++ backend/src/main/java/entities/Matchday.java
@@ -3,8 +3,10 @@ import javax.persistence.Entity;
 
 @Entity
 public class Matchday extends EntityGeneratedKey{
+	//TODO: Mapping
 	private int matchdayId;
 	
+	//TODO: Mapping
 	//FK
 	private int leagueId;
 }
