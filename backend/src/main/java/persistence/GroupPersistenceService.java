d9dfa84af8ad2360bbb19f549720aa72fee2bb1d
G Back <27279305+g-back@users.noreply.github.com>
2018-06-12 12:33:08 +0200

    added throws declaration

--- backend/src/main/java/persistence/GroupPersistenceService.java
+++ backend/src/main/java/persistence/GroupPersistenceService.java
@@ -24,7 +24,7 @@ public class GroupPersistenceService extends PersistenceService<Group> {
 	 * @return
 	 */
 	@SuppressWarnings("unchecked")
-	public List<Group> getSearch(String search) {
+	public List<Group> getSearch(String search) throws NoResultException {
 		return JPAOperations.doInJPA(this::entityManagerFactory, entityManager -> {
 			Query query = entityManager.createQuery("SELECT g FROM Group g WHERE groupName LIKE '%" + search + "%'");
 			List<Group> groups = query.getResultList();
