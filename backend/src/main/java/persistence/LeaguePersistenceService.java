419b3f8c1de17f65fb13ce4cae4518628f5c15ce
G Back <27279305+g-back@users.noreply.github.com>
2018-06-12 15:17:15 +0200

    added getCurrentLeague

--- backend/src/main/java/persistence/LeaguePersistenceService.java
+++ backend/src/main/java/persistence/LeaguePersistenceService.java
@@ -2,6 +2,10 @@ package persistence;
 
 import entities.League;
 
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+import java.util.List;
+
 /**
  *
  */
@@ -18,5 +22,16 @@ public class LeaguePersistenceService extends PersistenceService<League> {
         return instance = instance != null ? instance : new LeaguePersistenceService();
     }
 
+    public League getCurrentLeague() throws NoResultException{
+        return JPAOperations.doInJPA(this::entityManagerFactory, entityManager -> {
+            Query query = entityManager.createQuery("select l from league l order by l.season desc");
+            List<League> leagues = query.getResultList();
+            if (leagues.isEmpty()) {
+                throw new NoResultException();
+            } else {
+                return leagues.get(0);
+            }
+        });
+    }
 }
 
