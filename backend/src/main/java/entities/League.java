ed561c7b0a4397d239f1a73cb4144b11386917e4
G Back <27279305+g-back@users.noreply.github.com>
2018-06-12 14:36:42 +0200

    added currentmatchday as attribute

--- backend/src/main/java/entities/League.java
+++ backend/src/main/java/entities/League.java
@@ -3,6 +3,34 @@ import javax.persistence.Entity;
 
 @Entity
 public class League extends EntityGeneratedKey {
+
 	private int leagueId;
-	private String season;  //e.g. 2018
+
+	private String season;
+
+	private Matchday currentMatchday;
+
+	public int getLeagueId() {
+		return leagueId;
+	}
+
+	public void setLeagueId(int leagueId) {
+		this.leagueId = leagueId;
+	}
+
+	public String getSeason() {
+		return season;
+	}
+
+	public void setSeason(String season) {
+		this.season = season;
+	}
+
+	public Matchday getCurrentMatchday() {
+		return currentMatchday;
+	}
+
+	public void setCurrentMatchday(Matchday currentMatchday) {
+		this.currentMatchday = currentMatchday;
+	}
 }
