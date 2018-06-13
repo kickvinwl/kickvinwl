6bdbf729a3ec054002e140ea2d65a196d719d7b8
tobire <mail@tobias-reinke.de>
2018-06-13 16:22:17 +0200

    Bundesligatabelle mit Beispieldaten anzeigen

---
+++ frontend/script/bundesliga.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+	loadTabelle();
+});
+
+function loadTabelle() {
+	$.ajax({
+		url: urlPath + 'bundesliga.json',
+		type: 'GET',
+		success: function(data, textStatus, jqXHR) {
+			$.each(data.teams, function(i, val) {
+				$('#bundesligaTabelle tbody').append(`<tr>
+				  <th scope="row">${val.place}</th>
+				  <td><img src='${val.imageLink}' class="img-fluid" style="width: 20px;"> ${val.clubName}</td>
+				  <td>${val.games}</td>
+				  <td>${val.wins}</td>
+				  <td>${val.loses}</td>
+				  <td>${val.draws}</td>
+				  <td>${val.goals}:${val.opponentGoals}</td>
+				  <td>${val.difference}</td>
+				  <td>${val.points}</td>
+				</tr>`);
+			});
+		},
+		error: function(data) {
+			
+		}
+	});
+}
