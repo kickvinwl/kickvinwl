90e3a1ecad5c409bd46bc3cacd29079b4a29640f
G Back <27279305+g-back@users.noreply.github.com>
2018-06-13 09:34:43 +0200

    more changes

--- backend/src/main/java/persistence/BundesligaTableManager.java
+++ backend/src/main/java/persistence/BundesligaTableManager.java
@@ -12,11 +12,20 @@ public class BundesligaTableManager {
     private String apiParameters;
     private League league;
 
+    /**
+     * 
+     * @param league
+     */
     public BundesligaTableManager(League league) {
         apiParameters = String.valueOf(league.getLeagueId()) + "/" + league.getSeason().toString();
         this.league = league;
     }
 
+    /**
+     *
+     * @return
+     * @throws Exception
+     */
     private List<BundesligaTable> getBundesligatableFromAPI() throws Exception {
         BundesligaTableDeserializer bltd = new BundesligaTableDeserializer();
             List<BundesligaTable> ble = bltd.deserializeBundesligaTable(apiURL+apiParameters);
@@ -24,6 +33,10 @@ public class BundesligaTableManager {
             return ble;
     }
 
+    /**
+     *
+     * @return
+     */
     private List<BundesligaTable> getBundesligatableFromDatabase() {
         List<BundesligaTable> bl = BundesligaTablePersistenceService.getInstance().getAllEntriesByLeagueId(league.getId());
         return bl;
