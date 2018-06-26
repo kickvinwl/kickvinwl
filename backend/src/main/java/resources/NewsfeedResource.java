ea0279a66ebea51852bac26107c1b8f8c3a11382
G Back <27279305+g-back@users.noreply.github.com>
2018-06-26 12:17:15 +0200

    added getNews and postNews resources

--- backend/src/main/java/resources/NewsfeedResource.java
+++ backend/src/main/java/resources/NewsfeedResource.java
@@ -1,17 +1,33 @@
 package resources;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Map;
 
 @Path("/{a:news|News}")
 public abstract class NewsfeedResource {
     Response response;
 
+    /**
+     *
+     * @param token the session key of the user
+     * @return a response with either the requested data or a
+     * corresponding response code.
+     */
     @GET
     @Path("/{a:get|Get}")
     @Produces(MediaType.APPLICATION_JSON)
-    public abstract Response getNews(String token);
+    public abstract Response getNews(@QueryParam("token") String token);
+
+    /**
+     *
+     * @param input
+     * @return
+     */
+    @POST
+    @Path("/{a:post|Post}")
+    public abstract Response postNews(Map<String, String> input);
+
+
 }
--- backend/src/main/java/resources/NewsfeedResourceImpl.java
+++ backend/src/main/java/resources/NewsfeedResourceImpl.java
@@ -1,26 +1,44 @@
 package resources;
 
-import entities.NewsfeedMessage;
-import persistence.NewsfeedPersistenceService;
+import entities.User;
+import manager.NewsfeedManager;
 import persistence.UserPersistenceService;
 
 import javax.persistence.NoResultException;
 import javax.ws.rs.core.Response;
-import java.util.List;
+import java.util.Map;
 
-public class NewsfeedResourceImpl extends NewsfeedResource{
+public class NewsfeedResourceImpl extends NewsfeedResource {
 
     @Override
     public Response getNews(String token) {
         response = Response.accepted().build();
-        try{
-            //UserPersistenceService.getInstance().getBySessionKey(token);
-            List<NewsfeedMessage> news = NewsfeedPersistenceService.getInstance().getValidNewsfeedMessages();
-            response = Response.accepted(news.get(0)).build();
-        } catch ( NoResultException e1) {
-            response = Response.status(Response.Status.fromStatusCode(600)).build();
+        System.out.println("TOKEN = " + token);
+        try {
+            UserPersistenceService.getInstance().getBySessionKey(token);
+            NewsfeedManager newsManager = new NewsfeedManager();
+            response = Response.accepted(newsManager.getValidNews()).build();
+        } catch (NoResultException e1) {
+            response = Response.status(Response.Status.NO_CONTENT).build();
+        } catch (SecurityException e2) {
+            response = Response.status(Response.Status.UNAUTHORIZED).build();
         }
-
         return response;
+    }
+
+    @Override
+    public Response postNews(Map<String, String> input) {
+
+        String token = input.get("token");
+        try {
+            User user = UserPersistenceService.getInstance().getBySessionKey(token);
+            NewsfeedManager newsManager = new NewsfeedManager();
+            newsManager.postNewsMessage(input, user);
+        } catch (NoResultException exception) {
+            response = Response.status(Response.Status.UNAUTHORIZED).build();
+        } catch (SecurityException e) {
+            response = Response.status(Response.Status.NO_CONTENT).build();
+        }
+        return null;
     }
 }
