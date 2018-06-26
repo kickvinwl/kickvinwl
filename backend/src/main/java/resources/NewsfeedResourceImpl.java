09a5466da19d5aa9cf43b8243821079f6f5badc9
G Back <27279305+g-back@users.noreply.github.com>
2018-06-26 15:21:46 +0200

    added implementation of delete

--- backend/src/main/java/resources/NewsfeedResourceImpl.java
+++ backend/src/main/java/resources/NewsfeedResourceImpl.java
@@ -5,6 +5,7 @@ import manager.NewsfeedManager;
 import persistence.UserPersistenceService;
 
 import javax.persistence.NoResultException;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.core.Response;
 import java.text.ParseException;
 import java.util.Map;
@@ -34,13 +35,27 @@ public class NewsfeedResourceImpl extends NewsfeedResource {
         try {
             User user = UserPersistenceService.getInstance().getBySessionKey(token);
             NewsfeedManager newsManager = new NewsfeedManager();
-            newsManager.postNewsMessage(input, user);
-        } catch (NoResultException ex) {
+            newsManager.createNewsMessage(input, user);
+        } catch (NoResultException |SecurityException ex1) {
             response = Response.status(Response.Status.UNAUTHORIZED).build();
-        } catch (SecurityException ex1) {
-            response = Response.status(Response.Status.NO_CONTENT).build();
         } catch (ParseException ex2) {
             response = Response.status(Response.Status.BAD_REQUEST).build();
+        }
+        return response;
+    }
+
+    @Override
+    public Response deleteNews(Map<String, String> input, int newsId) {
+        response = Response.status(Response.Status.ACCEPTED).build();
+        String token = input.get("token");
+        try {
+            User user = UserPersistenceService.getInstance().getBySessionKey(token);
+            NewsfeedManager newsManager = new NewsfeedManager();
+            newsManager.deleteNewsMessage(newsId, user);
+        } catch (NotFoundException ex) {
+            response = Response.status(Response.Status.NOT_FOUND).build();
+        } catch (NoResultException | SecurityException ex2) {
+            response = Response.status(Response.Status.UNAUTHORIZED).build();
         }
         return response;
     }
