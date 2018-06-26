64a9fbdf3d8b4daae77caceb0da93154988d5d00
G Back <27279305+g-back@users.noreply.github.com>
2018-06-26 13:47:39 +0200

    added return of response code, another catch block for parse error

--- backend/src/main/java/resources/NewsfeedResourceImpl.java
+++ backend/src/main/java/resources/NewsfeedResourceImpl.java
@@ -6,6 +6,7 @@ import persistence.UserPersistenceService;
 
 import javax.persistence.NoResultException;
 import javax.ws.rs.core.Response;
+import java.text.ParseException;
 import java.util.Map;
 
 public class NewsfeedResourceImpl extends NewsfeedResource {
@@ -28,17 +29,19 @@ public class NewsfeedResourceImpl extends NewsfeedResource {
 
     @Override
     public Response postNews(Map<String, String> input) {
-
+        response = Response.status(Response.Status.ACCEPTED).build();
         String token = input.get("token");
         try {
             User user = UserPersistenceService.getInstance().getBySessionKey(token);
             NewsfeedManager newsManager = new NewsfeedManager();
             newsManager.postNewsMessage(input, user);
-        } catch (NoResultException exception) {
+        } catch (NoResultException ex) {
             response = Response.status(Response.Status.UNAUTHORIZED).build();
-        } catch (SecurityException e) {
+        } catch (SecurityException ex1) {
             response = Response.status(Response.Status.NO_CONTENT).build();
+        } catch (ParseException ex2) {
+            response = Response.status(Response.Status.BAD_REQUEST).build();
         }
-        return null;
+        return response;
     }
 }
