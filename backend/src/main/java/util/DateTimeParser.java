2470a2e3fb3a43f35a8dcc7fdf9a18c66534b26c
G Back <27279305+g-back@users.noreply.github.com>
2018-06-26 12:15:30 +0200

    added a date parser for the newsfeed resource

--- backend/src/main/java/util/DateTimeParser.java
+++ backend/src/main/java/util/DateTimeParser.java
@@ -1,4 +1,23 @@
 package util;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DateTimeParser {
+
+    /**
+     * This method parses a String to a Date Object, the
+     * format has been defined for the {@link resources.NewsfeedResource}.
+     * @param dateString String to be parsed to a Date
+     * @return the parsed Date
+     * @throws ParseException
+     */
+    public static Date parseNewsfeedDate(String dateString) throws ParseException {
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date newsfeedDate = df.parse((dateString));
+        return newsfeedDate;
+    }
+
 }
