77a23473c87e6558795ee2beb1ae780c0ccca596
Sergej Dick <sedic001@stud.fh-dortmund.de>
2018-06-26 15:22:53 +0200

    headchange

--- frontend/script/header.js
+++ frontend/script/header.js
@@ -7,10 +7,14 @@ $(document).ready(function(){
 	var token = Cookies.get('token');
 	if(typeof token != 'undefined'){
 		$.ajax({
-			url: 'backend/user/getImageOfUser?token=' + token,
+			url: 'backend/user/get/' + token,
 			type: 'GET',
 			success: function(result){
 				$('.dropdownMenuLink').attr("src", result.Picture);
+				if(result.userIsAdmin == "1")
+				{
+					$
+				}
 			},
 			error: function(response)
 			{
@@ -18,25 +22,19 @@ $(document).ready(function(){
 			}
 		});
 		$.ajax({
-			url: 'https://api.myjson.com/bins/pezia',
+			url: 'newsfeed-example.json',
 			type: 'GET',
 			success: function(result){
 				$.each(result, function(index, value){
-					var subTitle = "";
-					if(value.SubTitle == undefined)
-					{
-						subTitle = "";	
-					}
-					else
-						subTitle = value.SubTitle;
-					var r = `<div class="card m-1" style="width: 18rem;"><div class="card-body"><h5 class="card-title">
-					${index}</h5><h6 class="card-subtitle mb-2 text-muted">${subTitle}</h6>
-					<p class="card-text">${value.Text}</p>
-					</div>
-					</div>
-					`;
-					console.log(`${subTitle}`);
-					$('#newsFeed').append(r);
+					$.each(value, function(i, v){
+						var r = `<div class="card m-1" style="width: 18rem;"><div class="card-body"><h5 class="card-title">
+						${v.messageTitle}</h5>
+						<p class="card-text">${v.messageText}</p>
+						</div>
+						</div>
+						`;
+						$('#newsFeed').append(r);	
+					});
 				});
 			},
 			error: function(response){
@@ -44,6 +42,53 @@ $(document).ready(function(){
 			}
 		});
 	}
+
+	$('#NewsModal').ready(function(){
+		var today = new Date();
+		var month = today.getMonth() + 1;
+		if(month > 12)
+			month = 1;
+		var standard = "" + today.getFullYear() + "-" + checkLengthDate(month) + "-" + checkLengthDate(today.getDate()) + "T" + checkLengthDate(today.getHours()) + 
+					":" + checkLengthDate(today.getMinutes()) + ":" + checkLengthDate(today.getSeconds());
+		$('#startdatum').val(standard);
+		$('#enddatum').val(standard);
+	});
+
+	$('#btnNewsAnlegen').click(function(){
+		var title = $('#newsTitle').val();
+		var text = $('#newsText').val();
+		if(title === "" || text === "")
+		{
+			alert("Titel und Text dürfen nicht leer sein!");
+			return;
+		}
+		var startdatum = $('#startdatum').val();
+		var enddatum = $('#enddatum').val();
+		console.log("Startdatum: " + startdatum);
+		console.log("Enddatum: " + enddatum);
+		var token2 = Cookies.get('token');
+		if(token2 != 'undefined')
+		{
+			var dataToSend = new object();
+			dataToSend.messageText = text;
+			dataToSend.messageTitle = title;
+			dataToSend.startDate = startdatum;
+			dataToSend.endDate = enddatum;
+			dataToSend.token = token2;
+			dataToSend = JSON.stringify(dataToSend);
+			console.log(dataToSend);
+			/*$.ajax({
+				type: 'POST',
+				headers: 
+					{ 
+					    'Content-Type': 'application/json' 
+					},
+				url: '',
+				data: dataToSend,
+				dataType: "json"
+			});*/
+		}
+	});
 
 });
 
@@ -58,3 +103,9 @@ function logout() {
 	}
 	window.location.href = urlPath + '?page=login';
 };
+
+function checkLengthDate(input){
+	if(input.toString().length == 1)
+		input = "0" + input;
+	return input;
+}
