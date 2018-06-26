88db96326b337761e1e07c4bc5fcb9dccac4c24e
G Back <27279305+g-back@users.noreply.github.com>
2018-06-26 15:20:49 +0200

    added getByNewsId

--- backend/src/main/java/persistence/NewsfeedPersistenceService.java
+++ backend/src/main/java/persistence/NewsfeedPersistenceService.java
@@ -1,35 +1,57 @@
 package persistence;
 
-import entities.MatchTip;
 import entities.NewsfeedMessage;
 
 import javax.persistence.NoResultException;
 import javax.persistence.Query;
-import javax.persistence.TemporalType;
 import java.util.Date;
 import java.util.List;
 
-public class NewsfeedPersistenceService extends PersistenceService<NewsfeedMessage>{
+public class NewsfeedPersistenceService extends PersistenceService<NewsfeedMessage> {
 
-	   private static NewsfeedPersistenceService instance;
+    private static NewsfeedPersistenceService instance;
 
-	public static NewsfeedPersistenceService getInstance() {
-		return instance = instance != null ? instance : new NewsfeedPersistenceService();
-	}
+    public static NewsfeedPersistenceService getInstance() {
+        return instance = instance != null ? instance : new NewsfeedPersistenceService();
+    }
 
-	public List<NewsfeedMessage> getValidNewsfeedMessages() throws NoResultException {
-		return JPAOperations.doInJPA(this::entityManagerFactory, entityManager -> {
-			Query query = entityManager.createQuery(
-					"SELECT nm FROM NewsfeedMessage nm where nm.startDate < :now and nm.endDate > :now");
-			Date now = new Date();
-			query.setParameter("now", now);
-			List<NewsfeedMessage> newsfeedMessages = query.getResultList();
-			if (newsfeedMessages.isEmpty()) {
-				throw new NoResultException();
-			}
-			else {
-				return newsfeedMessages;
-			}
-		});
-	}
+    /**
+     *
+     * @return list of messages, that are still valid
+     * @throws NoResultException no valid messages have been foud
+     */
+    public List<NewsfeedMessage> getValidNewsfeedMessages() throws NoResultException {
+        return JPAOperations.doInJPA(this::entityManagerFactory, entityManager -> {
+            Query query = entityManager.createQuery(
+                    "SELECT nm FROM NewsfeedMessage nm where nm.startDate < :now and nm.endDate > :now");
+            Date now = new Date();
+            query.setParameter("now", now);
+            List<NewsfeedMessage> newsfeedMessages = query.getResultList();
+            if (newsfeedMessages.isEmpty()) {
+                throw new NoResultException();
+            } else {
+                return newsfeedMessages;
+            }
+        });
+    }
+
+    /**
+     *
+     * @param newsId id of the message
+     * @return NewsfeedMessage object
+     * @throws NoResultException no message with parameter id
+     */
+    public NewsfeedMessage getNewsfeedMessageById(final int newsId) throws NoResultException {
+        return JPAOperations.doInJPA(this::entityManagerFactory, entityManager -> {
+            Query query = entityManager.createQuery(
+                    "SELECT nm FROM NewsfeedMessage nm where nm.id = :newsId ");
+            query.setParameter("newsId", newsId);
+            List<NewsfeedMessage> newsfeedMessages = query.getResultList();
+            if (newsfeedMessages.isEmpty()) {
+                throw new NoResultException();
+            } else {
+                return newsfeedMessages.get(0);
+            }
+        });
+    }
 }
