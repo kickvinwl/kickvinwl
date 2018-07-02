3a8a14ba5e3ca11ebd472669805371be63934166
Christian Kok <christiankok@gmx.de>
2018-07-02 18:34:28 +0200

    Bestenliste für Gruppe suchen

--- backend/src/main/java/resources/LeaderboardResourceImpl.java
+++ backend/src/main/java/resources/LeaderboardResourceImpl.java
@@ -1,70 +1,120 @@
 package resources;
 
-import manager.MatchdayPointsCalculater;
+import java.util.List;
+
+import javax.persistence.NoResultException;
+import javax.ws.rs.core.Response;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import manager.MatchdayPointsCalculater;
+import persistence.GroupPersistenceService;
 import persistence.LeaderboardPersistenceService;
 import persistence.LeaguePersistenceService;
 import resources.datamodel.UserPoints;
 
-import javax.persistence.NoResultException;
-import javax.ws.rs.core.Response;
-import java.util.List;
-
 public class LeaderboardResourceImpl extends LeaderboardResource {
 
-    private Response response;
+	private Response response;
 
-    @Override
-    public Response getAlltimeLeaderboard(String groupName) {
-        response = Response.accepted().build();
-        Logger slf4jLogger = LoggerFactory.getLogger("alltime board logger");
-        slf4jLogger.debug("alltime board started");
-        try {
-            if(!groupName.equals("")){
-            List<UserPoints> leaderboard = LeaderboardPersistenceService.getInstance().getAlltimeLeaderboard(null);
-            }
-            else{
-                List<UserPoints> leaderboard = LeaderboardPersistenceService.getInstance().getAlltimeLeaderboard(GroupPersistenceService.getInstance().getByGroupName(groupName));
-            }
-            response = Response.accepted(leaderboard).build();
-        } catch (NoResultException e) {
-            response = Response.status(Response.Status.NOT_FOUND).build();
-        }
-        return response;
-    }
+	@Override
+	public Response calculateLeaderboard() {
+		MatchdayPointsCalculater.getInstance().updateUserPointsMatchday();
+		return Response.accepted().build();
+	}
 
-    @Override
-    public Response calculateLeaderboard() {
-        MatchdayPointsCalculater.getInstance().updateUserPointsMatchday();
-        return Response.accepted().build();
-    }
+	@Override
+	public Response getAlltimeLeaderboard() {
+		response = Response.accepted().build();
+		Logger slf4jLogger = LoggerFactory.getLogger("alltime board logger");
+		slf4jLogger.debug("alltime board started");
+		try {
+			List<UserPoints> leaderboard = LeaderboardPersistenceService.getInstance().getAlltimeLeaderboard();
+			response = Response.accepted(leaderboard).build();
+		} catch (NoResultException e) {
+			response = Response.status(Response.Status.NOT_FOUND).build();
+		}
+		return response;
+	}
 
-    @Override
-    public Response getSeasonLeaderboard() {
-        response = Response.accepted().build();
-        Logger slf4jLogger = LoggerFactory.getLogger("season board logger");
-        slf4jLogger.debug("season board started");
-        try {
-            List<UserPoints> leaderboard = LeaderboardPersistenceService.getInstance().getSeasonLeaderboard(LeaguePersistenceService.getInstance().getCurrentLeagueByLeagueId("bl1").getId());
-            response = Response.accepted(leaderboard).build();
-        } catch (NoResultException e) {
-            response = Response.status(Response.Status.NOT_FOUND).build();
-        }
-        return response;
-    }
+	@Override
+	public Response getSeasonLeaderboard() {
+		response = Response.accepted().build();
+		Logger slf4jLogger = LoggerFactory.getLogger("season board logger");
+		slf4jLogger.debug("season board started");
+		try {
+			List<UserPoints> leaderboard = LeaderboardPersistenceService.getInstance().getSeasonLeaderboard(
+					LeaguePersistenceService.getInstance().getCurrentLeagueByLeagueId("bl1").getId());
+			response = Response.accepted(leaderboard).build();
+		} catch (NoResultException e) {
+			response = Response.status(Response.Status.NOT_FOUND).build();
+		}
+		return response;
+	}
 
-    @Override
-    public Response getGamedayLeaderboard() {
-        response = Response.accepted().build();
-        Logger slf4jLogger = LoggerFactory.getLogger("gameday board logger");
-        slf4jLogger.debug("gameday board started");
-        try {
-            List<UserPoints> leaderboard = LeaderboardPersistenceService.getInstance().getGamedayLeaderboard(LeaguePersistenceService.getInstance().getCurrentLeagueByLeagueId("bl1").getCurrentMatchday().getId());
-            response = Response.accepted(leaderboard).build();
-        } catch (NoResultException e) {
-            response = Response.status(Response.Status.NOT_FOUND).build();
-        }
-        return response;
-    }
+	@Override
+	public Response getGamedayLeaderboard() {
+		response = Response.accepted().build();
+		Logger slf4jLogger = LoggerFactory.getLogger("gameday board logger");
+		slf4jLogger.debug("gameday board started");
+		try {
+			List<UserPoints> leaderboard = LeaderboardPersistenceService.getInstance()
+					.getGamedayLeaderboard(LeaguePersistenceService.getInstance().getCurrentLeagueByLeagueId("bl1")
+							.getCurrentMatchday().getId());
+			response = Response.accepted(leaderboard).build();
+		} catch (NoResultException e) {
+			response = Response.status(Response.Status.NOT_FOUND).build();
+		}
+		return response;
+	}
+
+	@Override
+	public Response getSeasonLeaderboardForGroup(String groupName) {
+		response = Response.accepted().build();
+		Logger slf4jLogger = LoggerFactory.getLogger("season board logger");
+		slf4jLogger.debug("season board started");
+		try {
+			List<UserPoints> leaderboard = LeaderboardPersistenceService.getInstance().getSeasonLeaderboard(
+					LeaguePersistenceService.getInstance().getCurrentLeagueByLeagueId("bl1").getId(),
+					GroupPersistenceService.getInstance().getByGroupName(groupName));
+			response = Response.accepted(leaderboard).build();
+		} catch (NoResultException e) {
+			response = Response.status(Response.Status.NOT_FOUND).build();
+		}
+		return response;
+	}
+
+	@Override
+	public Response getGamedayLeaderboardForGroup(String groupName) {
+		response = Response.accepted().build();
+		Logger slf4jLogger = LoggerFactory.getLogger("gameday board logger");
+		slf4jLogger.debug("gameday board started");
+		try {
+			List<UserPoints> leaderboard = LeaderboardPersistenceService.getInstance()
+					.getGamedayLeaderboard(
+							LeaguePersistenceService.getInstance().getCurrentLeagueByLeagueId("bl1")
+									.getCurrentMatchday().getId(),
+							GroupPersistenceService.getInstance().getByGroupName(groupName));
+			response = Response.accepted(leaderboard).build();
+		} catch (NoResultException e) {
+			response = Response.status(Response.Status.NOT_FOUND).build();
+		}
+		return response;
+	}
+
+	@Override
+	public Response getAlltimeLeaderboardForGroup(String groupName) {
+		response = Response.accepted().build();
+		Logger slf4jLogger = LoggerFactory.getLogger("alltime board logger");
+		slf4jLogger.debug("alltime board started");
+		try {
+			List<UserPoints> leaderboard = LeaderboardPersistenceService.getInstance()
+					.getAlltimeLeaderboard(GroupPersistenceService.getInstance().getByGroupName(groupName));
+			response = Response.accepted(leaderboard).build();
+		} catch (NoResultException e) {
+			response = Response.status(Response.Status.NOT_FOUND).build();
+		}
+		return response;
+	}
 }
