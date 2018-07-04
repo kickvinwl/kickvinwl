735100c696eff9719081c151d7d1546d0c0fb5f4
Mankianer <marvin.woelk@web.de>
2018-07-04 11:04:20 +0200

    Schedular für nach Match hinzugefügt

--- backend/src/main/java/util/Schedular.java
+++ backend/src/main/java/util/Schedular.java
@@ -1,24 +1,54 @@
 package util;
 
+import entities.Match;
+import persistence.MatchPersistenceService;
+
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Schedular {
 
-    ArrayList<Runnable> matchEndSchudls;
-    ScheduledExecutorService scheduledExecutorService;
+    private ArrayList<Runnable> matchEndSchudls;
+    private ScheduledExecutorService scheduledExecutorService;
+    private Match nextEndingMatch;
 
+    /**
+     * Verwaltet die verschiedenen Schedular aufgaben
+     */
     public Schedular() {
-
+        scheduledExecutorService = Executors.newScheduledThreadPool(1);
+        initMatchEndSchedul();
     }
 
     private void initMatchEndSchedul()
     {
         matchEndSchudls = new ArrayList<>();
-        scheduledExecutorService = Executors.newScheduledThreadPool(1);
+        nextEndingMatch = MatchPersistenceService.getInstance().getNextMatch();
+        scheduledExecutorService.schedule(new Runnable() {
+            @Override
+            public void run() {
+                nextEndingMatch = MatchPersistenceService.getInstance().getNextMatch();
+                matchEndSchudls.forEach(Runnable::run);
+
+                if(nextEndingMatch.getMatchDateTime().after(new Date())) {
+                    scheduledExecutorService.schedule(this, nextEndingMatch.getMatchDateTime().getTime() - System.currentTimeMillis() + 120 * 60 * 1000, TimeUnit.MILLISECONDS);
+                }
+                else
+                {
+                    System.out.println("keine Spiele in der Zukunft vorhanden!");
+                    scheduledExecutorService.schedule(this, 1, TimeUnit.MINUTES);
+                }
+            }
+        },nextEndingMatch.getMatchDateTime().getTime() - System.currentTimeMillis() + 120 * 60 * 1000, TimeUnit.MILLISECONDS);
     }
 
+    /**
+     *
+     * @param runnable wird 120 Minuten nach jedem Match ausgeführt
+     */
     public void addMatchEndSchedul(Runnable runnable)
     {
         matchEndSchudls.add(runnable);
